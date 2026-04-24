a4d10ec149e99ab3ff5e3cd4b7538e63a4c6e29e
ademceper <ceperadem@outlook.com>
2026-04-24 16:43:13 +0300

    thicken account header padding

--- apps/keycloak/src/account/root/Header.tsx
+++ apps/keycloak/src/account/root/Header.tsx
@@ -60,7 +60,7 @@ export const Header = () => {
 
   return (
     <header data-testid="page-header" className="bg-background">
-      <div className="mx-auto flex w-full max-w-5xl items-center gap-4 border-b px-4 py-2 sm:px-6 lg:px-8">
+      <div className="mx-auto flex w-full max-w-5xl items-center gap-4 border-b px-4 py-5 sm:px-6 lg:px-8">
         <a
           href={indexHref}
           className="inline-flex items-center text-2xl tracking-tight md:text-3xl"
